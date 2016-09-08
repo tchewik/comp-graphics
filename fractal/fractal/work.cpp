974fd8527a943132d903857c208851bada8b1769
tchewik <elenachistov@gmail.com>
2016-09-08 22:15:25 +0300

    drawing a circle with squares

--- fractal/fractal/work.cpp
+++ fractal/fractal/work.cpp
@@ -4,17 +4,17 @@
 #include <iostream>
 
 struct Point{
-	int x,
+	double x,
 		y;
 };
 
 class Square{
 private:
 	Point _points[4];
-	int _length;
-	double _angle;
+	double _length,
+		_angle;
 public:
-	Square(double x, double y, int length, double angle = 0.0){
+	Square(double x, double y, double length, double angle = 0.0){
 		_length = length;
 		_angle = angle;
 		_points[0].x = x;
@@ -26,17 +26,28 @@ public:
 		_points[3].x = _points[2].x - _length * cos(_angle);
 		_points[3].y = _points[2].y + _length * sin(_angle);
 	}
-	void draw(){
-		// accent the first point
-		setcolor(RED);
-		circle(_points[0].x, _points[0].y, 5);
+	void draw(int color){
+		// uncomment to accent the first point
+		//
+		// setcolor(RED);
+		// circle(_points[0].x, _points[0].y, 5);
 
 		// draw the square
-		setcolor(WHITE);
+		setcolor(color);
 		line(_points[0].x, _points[0].y, _points[1].x, _points[1].y);
 		line(_points[1].x, _points[1].y, _points[2].x, _points[2].y);
 		line(_points[2].x, _points[2].y, _points[3].x, _points[3].y);
 		line(_points[3].x, _points[3].y, _points[0].x, _points[0].y);
+	}
+	void draw_spiral(int times, int counter = 1){
+		_angle = _angle / counter;
+		draw(rand());
+		// uncomment to draw a boring white square
+		//draw(WHITE);
+		if (times != ++counter){
+			Square tmp(_points[1].x, _points[1].y, _length, _angle * counter);
+			tmp.draw_spiral(times, counter);
+		}
 	}
 };
 
@@ -44,8 +55,14 @@ int main(){
 	int gddriver = DETECT, gmode, errorcode;
 	initgraph(&gddriver, &gmode, "");
 
-	Square sqr(500, 500, 100);
-	sqr.draw();
+	// use negative angles
+	Square sqr(400, 300, 40, -0.6);
+
+	// uncomment to draw a single square
+	// sqr.draw();
+
+	// draw a chain of squares
+	sqr.draw_spiral(20);
 
 	_getch();
 	closegraph();
