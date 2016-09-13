dfc50a2ec345ad11624d8a791fc0340c156f50b5
tchewik <elenachistov@gmail.com>
2016-09-13 21:11:52 +0300

    added magic recursive function

--- fractal/fractal/work.cpp
+++ fractal/fractal/work.cpp
@@ -15,8 +15,8 @@ private:
 		_angle;
 public:
 	Square(double x, double y, double length, double angle = 0.0);
-	void draw(int color = WHITE);
-	void draw_circle(int times, int counter = 1, bool main = true);
+	void draw(int color = WHITE, bool highlight_first_point = false);
+	void draw_squares_recursive(int depth, int color = TX_PINK);
 };
 
 Square::Square(double x, double y, double length, double angle){
@@ -32,32 +32,26 @@ Square::Square(double x, double y, double length, double angle){
 	_points[3].y = _points[2].y + _length * sin(_angle);
 }
 
-void Square::draw(int color){
+void Square::draw(int color, bool highlight_first_point){
 	setcolor(color);
+	if (highlight_first_point)
+		circle(_points[0].x, _points[0].y, 5);
 	line(_points[0].x, _points[0].y, _points[1].x, _points[1].y);
 	line(_points[1].x, _points[1].y, _points[2].x, _points[2].y);
 	line(_points[2].x, _points[2].y, _points[3].x, _points[3].y);
 	line(_points[3].x, _points[3].y, _points[0].x, _points[0].y);
 }
 
-void Square::draw_circle(int times, int counter, bool main){
-	if (main){
-		_angle = _angle / counter;
-		draw_circle(times - counter + 1, 1, false);
-		if (times >= ++counter){
-			double angle = _angle < 0 ? _angle : _angle *= -1;
-			Square tmp(_points[1].x, _points[1].y, _length, _angle * counter);
-			tmp.draw_circle(times, counter);
-		}
-	}
-	else {
-		_angle = _angle / counter;
-		draw();
-		if (times >= ++counter){
-			double angle = _angle > 0 ? _angle : _angle *= -1;
-			Square tmp(_points[3].x, _points[3].y, _length, _angle * counter);
-			tmp.draw_circle(times, counter, false);
-		}
+void Square::draw_squares_recursive(int depth, int color){
+	draw(color);
+	if (--depth >= 0){
+		Square tmp(_points[3].x, _points[3].y, _length, _angle * 2);
+		tmp.draw_squares_recursive(depth);
+
+		double length = sqrt(pow(_points[2].x - tmp._points[1].x, 2) + pow(tmp._points[1].y - _points[2].y, 2));
+		double angle = 3.15159 / 2 - (3.14159 - tmp._angle - _angle) / 2;
+		Square tmp_0(_points[2].x, _points[2].y, length, angle);
+		tmp_0.draw_squares_recursive(depth);
 	}
 }
 
@@ -65,11 +59,8 @@ int main(){
 	int gddriver = DETECT, gmode, errorcode;
 	initgraph(&gddriver, &gmode, "");
 
-	Square sqr(400, 300, 40, -0.15);
-
-	// draw a chain of squares
-	sqr.draw_circle(8);
-	//sqr.draw_chain(6);
+	Square sqr(666, 666, 40, 0.666);
+	sqr.draw_squares_recursive(2);
 
 	_getch();
 	closegraph();
