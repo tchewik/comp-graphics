84da1ad62d2ae0ef4e84a6d1c1e1a36e29dc28f7
Elena Chistova <elenachistov@gmail.com>
2016-09-20 13:25:15 +0000

    ????

--- fractal/fractal/work.cpp
+++ fractal/fractal/work.cpp
@@ -8,11 +8,16 @@ int main(){
 	int gddriver = DETECT, gmode, errorcode;
 	initgraph(&gddriver, &gmode, "");
 
-	Square sqr(666, 666, 40, 0.666);
-	sqr.draw_squares_recursive(2);
+	Square sqr_0(666, 666, 80, 0);
+	//sqr_0.draw();
+	Square sqr_1(sqr_0._points[3].x, sqr_0._points[3].y, 80, 0.666);
+	//sqr_1.draw_squares_recursive(2, BLUE);
+	
+	
+	sqr_0.draw_squares_recursive(3, 0.3, BLUE);
 
-	Square sqr_0(100, 100, 20, 0.4);
-	sqr_0.draw_circle(3);
+	//Square sqr_0(100, 100, 20, 0.4);
+	//sqr_0.draw_circle(3);
 
 	_getch();
 	closegraph();
