741f9062899bfa46648096aec67ec02c7bf56d4e
tchewik <elenachistov@gmail.com>
2016-09-25 12:42:11 +0300

    added grad-to-rad function

--- fractal/fractal/work.cpp
+++ fractal/fractal/work.cpp
@@ -3,12 +3,17 @@
 #include <iostream>
 #include <fstream>
 #include "fractal.h"
+#define M_PI 3.14159265358979323846
+
+double toRad(double grad){
+	return grad * M_PI / 180;
+}
 
 int main(){
 	int gddriver = DETECT, gmode, errorcode;
 	initgraph(&gddriver, &gmode, "");
 
-	Square(520, 700, 120, 0).draw_squares_recursive(10, 1.0222, RED);
+	Square(520, 700, 120, 0).draw_squares_recursive(10, toRad(45), LIGHTGREEN);
 
 	_getch();
 	closegraph();
